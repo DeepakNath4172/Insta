89e40fedb00822c32c333889c7ec8b1dec9f3e14
DeepakNath4172 <nathd3191@gmail.com>
2022-11-29 17:50:35 +0530

    Done

---
+++ Java.js
@@ -0,0 +1,9 @@
+let login_id = "nathd3191@gmail.com";
+let cloud_id = "nathd3191@gmail.com";
+let login_pass = 1234;
+let cloud_pass = 1234;
+if((login_id == cloud_id)) && (login_pass == cloud_pass)){
+	console.log("Access Granted");
+}else{
+	console.log("Access Denied);
+}
